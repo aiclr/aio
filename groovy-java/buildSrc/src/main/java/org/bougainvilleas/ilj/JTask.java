2c077367686e99a4f781d11324a590a8522c1e17
caddy <caddyren@qq.com>
2022-09-30 13:51:57 +0000

    feat: gradle task in buildSrc

---
+++ groovy-java/buildSrc/src/main/java/org/bougainvilleas/ilj/JTask.java
@@ -0,0 +1,30 @@
+package org.bougainvilleas.ilj;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.MapProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+
+public abstract class JTask extends DefaultTask {
+
+  @Input
+  public abstract ListProperty<String> getSkip();
+
+  @Input
+  public abstract MapProperty<String,String> getMap();
+
+  @Input
+  public abstract Property<String> getMessage();
+
+  @Input
+  public abstract Property<String> getGreeter();
+
+  @TaskAction
+  void javaGreet(){
+    System.out.println(this.getMap().get());
+    System.out.println(this.getSkip().get());
+    System.out.println(getMessage().get() +" from "+getGreeter().get());
+  }
+}
