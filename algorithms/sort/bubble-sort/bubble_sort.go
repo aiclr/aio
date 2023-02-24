7e4d91a0878e11c5a167496078d11f398db6cd33
caddy <caddyren@qq.com>
2023-02-24 17:38:17 +0800

    algorithms: Bubble Sort review

--- algorithms/sort/bubble-sort/bubble_sort.go
+++ algorithms/sort/bubble-sort/bubble_sort.go
@@ -1,18 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
+// go run bubble_sort.go 10
 func main() {
-	mock := [10]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-	length := len(mock)
+
+	length, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	mock := randomArray(length)
+	bubble(mock, length)
+}
+
+// 异或运算 交换位置
+func swap(first *int, next *int) {
+	*first = *first ^ *next
+	*next = *first ^ *next
+	*first = *first ^ *next
+}
+
+func randomArray(length int) []int {
+	// 设置随机数种子
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	result := make([]int, length)
+
 	for i := 0; i < length; i++ {
-		for j := 0; j < length-1-i; j++ {
-			if mock[j] > mock[j+1] {
-				mock[j] = mock[j] ^ mock[j+1]
-				mock[j+1] = mock[j] ^ mock[j+1]
-				mock[j] = mock[j] ^ mock[j+1]
+		result[i] = r1.Intn(length)
+	}
+	return result
+}
+
+func bubble(arr []int, length int) {
+	// 已有序则快速结束
+	sorted := true
+	for i := 1; i < length && sorted; i++ {
+		sorted = false
+		for j := 0; j < length-i; j++ {
+			if arr[j] > arr[j+1] {
+				swap(&arr[j], &arr[j+1])
+				sorted = true
 			}
 		}
+		fmt.Println(arr)
 	}
-	fmt.Println(mock)
 }
--- algorithms/sort/bubble-sort/bubble_sort.groovy
+++ algorithms/sort/bubble-sort/bubble_sort.groovy
@@ -1,16 +1,57 @@
-int[] mock = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-
-void bubbleSort(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-        for (int j = 0; j < arr.length - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                arr[j]=arr[j]^arr[j+1]
-                arr[j+1]=arr[j]^arr[j+1]
-                arr[j]=arr[j]^arr[j+1]
-            }
-        }
-    }
-    System.out.println(arr)
+int[] randomArray(int length){
+    Random r=new Random();
+    int[] result=new int[length];
+    for(int i=0;i<length;i++)
+        result[i]=r.nextInt(length);
+    return result;
 }
 
-bubbleSort(mock)
+void bubbleSort(int[] arr) {
+    boolean bool=true;
+    for (int i=1;i<arr.length && bool;i++) {
+        bool=false;
+        for (int j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                arr[j]=arr[j]^arr[j+1];
+                arr[j+1]=arr[j]^arr[j+1];
+                arr[j]=arr[j]^arr[j+1];
+                bool=true;
+            }
+        }
+        System.out.println(arr);
+    }
+}
+
+bubbleSort(randomArray(Integer.valueOf(this.args[0])))
+// groovy bubble_sort.groovy 10
+
+println();
+
+def randomArrayGroovy(int length){
+    def r=new Random()
+    def result=new int[length]
+
+    length.times {
+        result[it] = r.nextInt(length) 
+    }
+        
+    return result
+}
+
+def bubbleSortGroovy(int[] arr) {
+    def bool=true
+    for (def i=1;i<arr.length && bool;i++) {
+        bool=false
+        0.step(arr.length - i, 1) { j ->
+            if (arr[j] > arr[j + 1]) {
+                arr[j] = arr[j] ^ arr[j + 1]
+                arr[j + 1] = arr[j] ^ arr[j + 1]
+                arr[j] = arr[j] ^ arr[j + 1]
+                bool = true
+            }
+        }
+        println(arr)
+    }
+}
+
+bubbleSortGroovy(randomArrayGroovy(Integer.valueOf(this.args[0])))
