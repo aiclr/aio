a222a50a2941860d3f1a95a46be9adead84433ee
caddy <caddyren@qq.com>
2023-02-28 15:21:13 +0800

    spring: 类注解切点 @within 代替 @target 使用

--- spring/src/main/java/org/bougainvilleas/spring/aop/annotation/AOPAnnotation/TypeAopAspect.java
+++ spring/src/main/java/org/bougainvilleas/spring/aop/annotation/AOPAnnotation/TypeAopAspect.java
@@ -17,8 +17,16 @@ public class TypeAopAspect {
 
   /**
    * 切入点 带有 @PK 注解的方法
+   * @target 匹配的是 执行方法 的对象的class上是否有注解
+   * @within 匹配的是 定义方法 的对象的class上是否有注解
+   * 由于运行时多态的特性，执行方法的对象和定义方法的对象可能不是同一个对象
+   * 定义方法的类是可以提前知道的，这个是静态的，但是执行方法的类是无法提前知道的，是运行时动态的，
+   * 因此，在@target的场景下，spring aop给所有的bean都生成了代理类，如果无法生成代理就会报错。
+   * 比如：cglib遇到final的时候
+   * @target一定要慎用
    */
-  @Pointcut(value = "@target(org.bougainvilleas.spring.aop.annotation.AOPAnnotation.TypeAOP) && args(vo)")
+//  @Pointcut(value = "@target(org.bougainvilleas.spring.aop.annotation.AOPAnnotation.TypeAOP) && args(vo)")
+  @Pointcut(value = "@within(org.bougainvilleas.spring.aop.annotation.AOPAnnotation.TypeAOP) && args(vo)")
   public <T extends Number> void yunPKPoint(T vo)
   {
   }
