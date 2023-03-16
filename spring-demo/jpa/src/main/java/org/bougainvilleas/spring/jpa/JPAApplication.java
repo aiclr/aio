20f1b0195225ea27cc36107c90f482fb958caef4
caddy <caddyren@qq.com>
2023-03-16 11:20:29 +0800

    jpa： pageable 分页查询

--- spring-demo/jpa/src/main/java/org/bougainvilleas/spring/jpa/JPAApplication.java
+++ spring-demo/jpa/src/main/java/org/bougainvilleas/spring/jpa/JPAApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
 public class JPAApplication {
@@ -34,22 +37,40 @@ public class JPAApplication {
             for (Person person : repository.findAll()) {
                 log.info(person.toString());
             }
-            log.info("");
 
             // fetch an individual person by ID
             Person person = repository.findById(1L);
             log.info("person found with findById(1L):");
             log.info("--------------------------------");
             log.info(person.toString());
-            log.info("");
 
             // fetch persons by last name
             log.info("person found with findByLastName('Bauer'):");
             log.info("--------------------------------------------");
-            repository.findByLastName("Bauer").forEach(bauer -> {
-                log.info(bauer.toString());
-            });
-            log.info("");
+            repository.findByLastName("Bauer").forEach(bauer -> log.info(bauer.toString()));
+
+            // fetch persons by last name
+            log.info("person found with findAllByPage():");
+            log.info("--------------------------------------------");
+            Pageable pageable= PageRequest.of(0,2);
+            Page<Person> personPage = repository.findAll(pageable);
+            log.info("TotalPages={},TotalElements={}",personPage.getTotalPages(),personPage.getTotalElements());
+            personPage.forEach(bauer -> log.info(bauer.toString()));
+
+            // fetch persons by last name
+            log.info("person found with findAllByPageHql():");
+            log.info("--------------------------------------------");
+            Page<Person> personPage1 = repository.findAllHql(pageable);
+            log.info("TotalPages={},TotalElements={}",personPage1.getTotalPages(),personPage1.getTotalElements());
+            personPage1.forEach(bauer -> log.info(bauer.toString()));
+
+            // fetch persons by last name
+            log.info("person found with findAllByPageSql():");
+            log.info("--------------------------------------------");
+            Page<Person> personPage2 = repository.findAllSql(pageable);
+            log.info("TotalPages={},TotalElements={}",personPage2.getTotalPages(),personPage2.getTotalElements());
+            personPage2.forEach(bauer -> log.info(bauer.toString()));
+
         };
     }
 }
--- spring-demo/jpa/src/main/java/org/bougainvilleas/spring/jpa/PersonRepository.java
+++ spring-demo/jpa/src/main/java/org/bougainvilleas/spring/jpa/PersonRepository.java
@@ -1,10 +1,27 @@
 package org.bougainvilleas.spring.jpa;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface PersonRepository extends CrudRepository<Person,Long> {
-    List<Person> findByLastName(String lastName);
-    Person findById(long id);
+public interface PersonRepository extends CrudRepository<Person, Long> {
+  List<Person> findByLastName(String lastName);
+
+  Person findById(long id);
+
+  Page<Person> findAll(Pageable pageable);
+
+  @Query("select p from Person p")
+  Page<Person> findAllHql(Pageable pageable);
+
+  /**
+   * nativeQuery = true 执行原生SQL
+   */
+  @Query(value = "select * from person",
+          countQuery = "select count(id) from person",
+          nativeQuery = true)
+  Page<Person> findAllSql(Pageable pageable);
 }
