1d06bcd5cc1fca5687fe31e0d7635ec5ad727f1d
caddy <caddyren@qq.com>
2023-03-17 16:18:53 +0800

    spring: upgrade springboot from 2.6.4 to 2.7.9
    - @Deprecated org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter

--- spring-guides/authenticating-ldap/src/main/java/org/bougainvilleas/spring/authenticatingldap/WebSecurityConfig.java
+++ spring-guides/authenticating-ldap/src/main/java/org/bougainvilleas/spring/authenticatingldap/WebSecurityConfig.java
@@ -1,24 +1,34 @@
 package org.bougainvilleas.spring.authenticatingldap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
             .authorizeRequests()
             .anyRequest().fullyAuthenticated()
             .and()
             .formLogin();
+
+    return http.build();
   }
 
-  @Override
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().antMatchers("/resources/**");
+  }
+
+  @Autowired
   public void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
             .ldapAuthentication()
@@ -31,4 +41,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .passwordEncoder(new BCryptPasswordEncoder())
             .passwordAttribute("userPassword");
   }
+
 }
