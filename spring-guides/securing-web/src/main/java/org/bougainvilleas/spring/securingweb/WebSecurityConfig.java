6684848c73e9b9e363ef5b43c4369c62ef2552c1
caddy <caddyren@qq.com>
2023-04-09 12:25:12 +0800

    spring: deal Deprecated for spring security

--- spring-guides/securing-web/src/main/java/org/bougainvilleas/spring/securingweb/WebSecurityConfig.java
+++ spring-guides/securing-web/src/main/java/org/bougainvilleas/spring/securingweb/WebSecurityConfig.java
@@ -4,40 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-            .authorizeRequests()
-            .antMatchers("/", "/home").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll();
+            .authorizeHttpRequests((requests) -> requests
+//                    .requestMatchers("/", "/home").permitAll()
+                    .mvcMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin((form) -> form
+                    .loginPage("/login")
+                    .permitAll()
+            )
+            .logout((logout) -> logout.permitAll());
+
+    return http.build();
   }
 
   @Bean
-  @Override
   public UserDetailsService userDetailsService() {
-    UserDetails user =
-            User.withDefaultPasswordEncoder()
-                    .username("user")
-                    .password("password")
+
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    System.out.println(encoder.encode("password"));
+
+    UserDetails user = User.withUsername("user")
+                    .password("{bcrypt}$2a$10$tNcVIM5.WNRq0liUxmpItemrT24buXX7RSHVjQB9IQnkkBRlOK1Na")
                     .roles("USER")
                     .build();
-
     return new InMemoryUserDetailsManager(user);
   }
+
+
 }
