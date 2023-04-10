4385c56de3db5e730c6d19ae3addbdaedbdfa8b4
caddy <caddyren@qq.com>
2023-04-10 18:16:06 +0800

    spring: Handling Form Submission

--- spring-guides/serving-web-content/src/main/java/org/bougainvilleas/spring/servingwebcontent/GreetingController.java
+++ spring-guides/serving-web-content/src/main/java/org/bougainvilleas/spring/servingwebcontent/GreetingController.java
@@ -3,6 +3,8 @@ package org.bougainvilleas.spring.servingwebcontent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,6 +14,18 @@ public class GreetingController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/greeting-form")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting-form";
+    }
+
+    @PostMapping("/greeting-form")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
     }
 
 }
--- spring-guides/serving-web-content/src/test/java/org/bougainvilleas/spring/servingwebcontent/ServingWebContentApplicationTest.java
+++ spring-guides/serving-web-content/src/test/java/org/bougainvilleas/spring/servingwebcontent/ServingWebContentApplicationTest.java
@@ -2,6 +2,7 @@ package org.bougainvilleas.spring.servingwebcontent;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,9 +10,11 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = GreetingController.class)
+@TestPropertySource(properties = "logging.level.org.springframework.web=DEBUG")
 public class ServingWebContentApplicationTest {
 
     @Autowired
@@ -37,6 +40,21 @@ public class ServingWebContentApplicationTest {
     public void greetingWithUser() throws Exception {
         mockMvc.perform(get("/greeting").param("name", "Greg"))
                 .andExpect(content().string(containsString("Hello, Greg!")));
+    }
+
+    @Test
+    @DisplayName("GET /greeting-form")
+    public void rendersForm() throws Exception {
+        mockMvc.perform(get("/greeting-form"))
+                .andExpect(content().string(containsString("Form")));
+    }
+
+    @Test
+    @DisplayName("POST /greeting-form")
+    public void submitsForm() throws Exception {
+        mockMvc.perform(post("/greeting-form").param("id", "12345").param("content", "Hello"))
+                .andExpect(content().string(containsString("Result")))
+                .andExpect(content().string(containsString("id: 12345")));
     }
 
 }
