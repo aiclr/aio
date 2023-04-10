af1c914d2384bb9fbc3c97fece856c2443d2fd30
caddy <caddyren@qq.com>
2023-04-10 17:50:43 +0800

    spring: Serving Web Content with Spring MVC

---
+++ spring-guides/serving-web-content/README.md
@@ -0,0 +1,6 @@
+# Serving Web Content with Spring MVC
+
+# 参考文档
+
+- [spring guides](https://spring.io/guides/gs/serving-web-content/)
+- [spring demo](https://github.com/spring-guides/gs-serving-web-content.git)
---
+++ spring-guides/serving-web-content/src/test/java/org/bougainvilleas/spring/servingwebcontent/ServingWebContentApplicationTest.java
@@ -0,0 +1,42 @@
+package org.bougainvilleas.spring.servingwebcontent;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = GreetingController.class)
+public class ServingWebContentApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/index.html")
+    public void homePage() throws Exception {
+        // N.B. jsoup can be useful for asserting HTML content
+        mockMvc.perform(get("/index.html"))
+                .andExpect(content().string(containsString("Get your greeting")));
+    }
+
+    @Test
+    @DisplayName("/greeting")
+    public void greeting() throws Exception {
+        mockMvc.perform(get("/greeting"))
+                .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+    @Test
+    @DisplayName("/greeting?name=Greg")
+    public void greetingWithUser() throws Exception {
+        mockMvc.perform(get("/greeting").param("name", "Greg"))
+                .andExpect(content().string(containsString("Hello, Greg!")));
+    }
+
+}
