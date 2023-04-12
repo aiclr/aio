af83b0058640b77f2affb36b7f5ac16dc7fa2ad7
caddy <caddyren@qq.com>
2023-04-12 18:36:35 +0800

    rust: formatted print

--- rust/rust-by-example/hello-world/hello.rs
+++ rust/rust-by-example/hello-world/hello.rs
@@ -1,3 +1,8 @@
+// Line comments which go to the end of the line.
+/* Block comments which go to the closing delimiter. */
+/// Generate library docs for the following item.  //! Generate library docs for the enclosing item.
+
+
 // This is a comment(注释), and is ignored by the compiler
 // You can test this code by clicking the "Run" button over there ->
 // or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
@@ -10,7 +15,64 @@ fn main(){
     // Statements here are executed when the compiled binary is called
     // Print text to the console
     println!("Hello World!");
-    println!("I'm a Rustacean!")
+    println!("I'm a Rustacean!");
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+    /* formatted print */
+    // In general, the `{}` will be automatically replaced with any arguments. These will be stringified.
+    println!("{} days",31);
+    // Positional arguments can be used. Specifying an integer inside `{}`
+    // determines which additional argument will be replaced. Arguments start
+    // at 0 immediately after the format string.
+    println!("{0}, this is {1}. {1}, this is {0}","Alice","Bob");
+    //As can named arguments
+    println!("{subject} {verb} {object}",
+    object="the lazy dog",
+    subject="the quick brown fox",
+    verb="jumps over");
+    //Different formatting can be invoked by specifying the format character after a `:`.
+    println!("Base 10:                        {}",69420);//69420
+    println!("Base 2 (binary):                {:b}",69420);//10000111100101100
+    println!("Base 8 (octal):                 {:o}",69420);//207454
+    println!("Base 16 (hexadecimal):          {:x}",69420);//10f2c
+    println!("Base 16 (hexadecimal):          {:X}",69420);//10F2C
+
+    //you can right-justify text with a specified width. This will output "    1".(Four white spaces and a "1",for a total width of 5)
+    println!("{number:>5}",number=1);
+    // You can pad numbers with extra zeroes,
+    // and left-adjust by flipping the sign. This will output "10000".
+    println!("{number:0<5}",number=1);
+    // You can use named arguments in the format specifier by appending a `$`
+    println!("{number:#>width$}",number=1,width=5);
+
+    // Rust even checks to make sure the correct number of arguments are used.
+    println!("My name is {0}, {1} {0}", "Bond","James");
+    // FIXME ^ Add the missing argument: "James"
+
+    // Only types that implement fmt::Display can be formatted with `{}`. User-
+    // defined types do not implement fmt::Display by default.
+
+    #[allow(dead_code)] // disable `dead_code` which warn against unused module
+    struct Structure(i32);
+
+    // This will not compile because `Structure` does not implement
+    // fmt::Display.
+    println!("This struct `{}` won't print...", Structure(3).0);
+    // TODO ^ Try uncommenting this line https://doc.rust-lang.org/stable/rust-by-example/custom_types/structs.html
+
+    // For Rust 1.58 and above, you can directly capture the argument from a
+    // surrounding variable. Just like the above, this will output
+    // "    1", 4 white spaces and a "1".
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!("{number:0>width$}");
+
+    //https://doc.rust-lang.org/std/fmt/
+    let pi=3.141592;
+    println!("Pi is roughly {pi:.3}")
+
 }
 //rustc hello.rs -o hello.out
 //./hello.out
