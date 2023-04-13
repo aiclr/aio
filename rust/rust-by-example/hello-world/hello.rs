2dd48c992f485fcb718d318302bae3cf47c61032
caddy <caddyren@qq.com>
2023-04-13 16:49:15 +0800

    rust: format

--- rust/rust-by-example/hello-world/hello.rs
+++ rust/rust-by-example/hello-world/hello.rs
@@ -11,7 +11,7 @@
 // You can always return to the original(起初的) code by clicking the "Reset" button ->
 
 // This is the main function
-fn main(){
+fn main() {
     // Statements here are executed when the compiled binary is called
     // Print text to the console
     println!("Hello World!");
@@ -22,33 +22,33 @@ fn main(){
 
     /* formatted print */
     // In general, the `{}` will be automatically replaced with any arguments. These will be stringified.
-    println!("{} days",31);
+    println!("{} days", 31);
     // Positional arguments can be used. Specifying an integer inside `{}`
     // determines which additional argument will be replaced. Arguments start
     // at 0 immediately after the format string.
-    println!("{0}, this is {1}. {1}, this is {0}","Alice","Bob");
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
     //As can named arguments
     println!("{subject} {verb} {object}",
-    object="the lazy dog",
-    subject="the quick brown fox",
-    verb="jumps over");
+             object = "the lazy dog",
+             subject = "the quick brown fox",
+             verb = "jumps over");
     //Different formatting can be invoked by specifying the format character after a `:`.
-    println!("Base 10:                        {}",69420);//69420
-    println!("Base 2 (binary):                {:b}",69420);//10000111100101100
-    println!("Base 8 (octal):                 {:o}",69420);//207454
-    println!("Base 16 (hexadecimal):          {:x}",69420);//10f2c
-    println!("Base 16 (hexadecimal):          {:X}",69420);//10F2C
+    println!("Base 10:                        {}", 69420);//69420
+    println!("Base 2 (binary):                {:b}", 69420);//10000111100101100
+    println!("Base 8 (octal):                 {:o}", 69420);//207454
+    println!("Base 16 (hexadecimal):          {:x}", 69420);//10f2c
+    println!("Base 16 (hexadecimal):          {:X}", 69420);//10F2C
 
     //you can right-justify text with a specified width. This will output "    1".(Four white spaces and a "1",for a total width of 5)
-    println!("{number:>5}",number=1);
+    println!("{number:>5}", number = 1);
     // You can pad numbers with extra zeroes,
     // and left-adjust by flipping the sign. This will output "10000".
-    println!("{number:0<5}",number=1);
+    println!("{number:0<5}", number = 1);
     // You can use named arguments in the format specifier by appending a `$`
-    println!("{number:#>width$}",number=1,width=5);
+    println!("{number:#>width$}", number = 1, width = 5);
 
     // Rust even checks to make sure the correct number of arguments are used.
-    println!("My name is {0}, {1} {0}", "Bond","James");
+    println!("My name is {0}, {1} {0}", "Bond", "James");
     // FIXME ^ Add the missing argument: "James"
 
     // Only types that implement fmt::Display can be formatted with `{}`. User-
@@ -70,9 +70,8 @@ fn main(){
     println!("{number:0>width$}");
 
     //https://doc.rust-lang.org/std/fmt/
-    let pi=3.141592;
+    let pi = 3.141592;
     println!("Pi is roughly {pi:.3}")
-
 }
 //rustc hello.rs -o hello.out
 //./hello.out
