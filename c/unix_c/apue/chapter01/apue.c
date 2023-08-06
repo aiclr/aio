5a83ace8bef8fc90f71ff5870342e73c24d7b7de
caddy <caddyren@qq.com>
2023-08-06 09:28:32 +0800

    apue: simple shell

--- c/unix_c/apue/chapter01/apue.c
+++ c/unix_c/apue/chapter01/apue.c
@@ -1,5 +1,33 @@
 #include "apue.h"
 
+/**
+ * man 3 va_list
+ * man 3 va_start
+ * man 3 va_end
+ * ISO C variable arguments
+ */
+#include <stdarg.h>
+
+/**
+ * man 3 errno
+ * for definition of errno
+ */
+#include <errno.h>
+
+static void err_doit(int errnoflag,int error,const char *fmt,va_list ap);
+
+/**
+ * Nonfatal error related to a system call
+ * print a messagge and return
+ */
+void err_ret(const char * fmt,...)
+{
+	va_list ap;
+	va_start(ap,fmt);
+	err_doit(1,errno,fmt,ap);
+	va_end(ap);
+}
+
 void err_quit(const char * fmt,...)
 {
   va_list ap;
--- c/unix_c/apue/chapter01/apue.h
+++ c/unix_c/apue/chapter01/apue.h
@@ -1,19 +1,6 @@
 #ifndef _CHAPTER01_H
 #define _CHAPTER01_H
 
-/**
- * man 3 va_list
- * man 3 va_start
- * man 3 va_end
- */
-#include <stdarg.h>
-/**
- * man 3 errno
- */
-#include <errno.h>
-
-
-
 
 /**
  * man NULL
@@ -45,8 +32,8 @@
 #define MAXLINE 4096 /* max line length */
 
 
-static void err_doit(int,int,const char *,va_list); 
 void err_quit(const char*,...) __attribute__((noreturn));
+void err_ret(const char*,...);
 void err_sys(const char*,...) __attribute__((noreturn));
 
 #endif /* _CHAPTER01_H */
